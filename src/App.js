d32b43b85509bb29ae003363f17b196551933cca
RugvedDhorje <rdhorje6@gmail.com>
2024-09-13 16:26:35 +0530

    signin/signup form && login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-[100px] bg-yellow-500 text-white text-center font-bold">hello i am back</h1>
     </div>
   );
 }
